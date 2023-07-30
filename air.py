14c21f105762a0d87231548cf5e226bae6b04962
syifamelinda <109504720+syifamelinda@users.noreply.github.com>
2023-07-30 23:21:05 +0700

    Update air.py

--- air.py
+++ air.py
@@ -74,7 +74,7 @@ if selected == "Beranda":
   # Tampilkan keterangan tambahan
     st.markdown(
     "<center>Masukkan data kandungan air minum anda, untuk mengetahui kelayakannya!</center>"
-    "<center>Sebelumnya silahkan membaca buku panduannya terlebih dahulu <a href='https://www.example.com/buku_panduan.pdf' target='_blank'>disini</a>!</center>"
+    "<center>Sebelumnya silahkan membaca buku panduannya terlebih dahulu <a href='https://drive.google.com/drive/folders/1LvNsC55XPSoXlDhV0L36W8pM2YjZvEBb?usp=sharing' target='_blank'>disini</a>!</center>"
     "<hr style='border: 1px solid black;'><br>",
     unsafe_allow_html=True
 )
